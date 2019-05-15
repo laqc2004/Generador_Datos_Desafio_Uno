f8e5b94771daf4c752f6e081f21ec68ebd97add4
laqc2004 <laqc2004@gmail.com>
2019-05-14 20:24:36 -0400

    Se usa persistencia de clases
    Se agrega la implementacion del servicio rest

--- src/main/java/com/desafio/demo/DesafioApplication.java
+++ src/main/java/com/desafio/demo/DesafioApplication.java
@@ -3,16 +3,12 @@ package com.desafio.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.desafio.motor.Desafio;
 
 @SpringBootApplication
 public class DesafioApplication {
 
-	public static void main(String[] args) {
-		Desafio desarrollo= new Desafio();
-		desarrollo.desarrollo();
-		
-	//	SpringApplication.run(DesafioApplication.class, args);
+	public static void main(String[] args) {		
+		SpringApplication.run(DesafioApplication.class, args);
 	}
 
 }
